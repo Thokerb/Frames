4963d109f510311c66f9253d18d6761d9a321004
Thokerb <Thomas.Kerber@student.uibk.ac.at>
2025-07-16 14:16:18 +0200

    Add dark mode toggle and menu bar to the application

--- Frames.Museum.WebApp/src/app/app.ts
+++ Frames.Museum.WebApp/src/app/app.ts
@@ -1,14 +1,69 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Button} from 'primeng/button';
 import {Card} from 'primeng/card';
+import {Menubar} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import Aura from '@primeuix/themes/aura';
+import {ToggleSwitch} from 'primeng/toggleswitch';
+import {FormsModule} from '@angular/forms';
+import {PrimeNG} from 'primeng/config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button, Card],
+  imports: [RouterOutlet, Button, Card, Menubar, ToggleSwitch, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Frames.Museum.WebApp');
+  primeNgConfig = inject(PrimeNG);
+  isDarkMode = signal(false);
+
+  constructor() {
+    this.primeNgConfig.theme.set({
+      preset: Aura,
+      options: {
+        darkModeSelector: '.dark',
+      },
+    });
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element?.classList.toggle('dark');
+    this.isDarkMode.set(!this.isDarkMode());
+  }
+
+  items: MenuItem[] = [
+    {
+      label: 'Router',
+      icon: 'pi pi-palette',
+      items: [
+        {
+          label: 'Installation',
+          routerLink: '/installation'
+        },
+        {
+          label: 'Configuration',
+          routerLink: '/configuration'
+        }
+      ]
+    },
+    {
+      label: 'External',
+      icon: 'pi pi-home',
+      items: [
+        {
+          label: 'Angular',
+          url: 'https://angular.io/'
+        },
+        {
+          label: 'Vite.js',
+          url: 'https://vitejs.dev/'
+        }
+      ]
+    }
+  ];
+
 }
