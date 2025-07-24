29c6439000a9735b2b6e58a398691f804229e555
Thokerb <Thomas.Kerber@student.uibk.ac.at>
2025-07-25 00:19:46 +0200

    found serialization error

---
+++ Frames.Test2/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Frames.Model;
+using Frames.Museum.BlinkingLightBRTest;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Frames.Test2;
+
+public class SerializationTest
+{
+    [Fact]
+    public void TestSerialization()
+    {
+        // Arrange
+        ICoupledModel arena = new CArena();
+
+        // Act
+        var json = JsonConvert.SerializeObject(arena, new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Error // or Ignore
+        });
+        JsonConvert.SerializeObject(new BlinkingLightAtomicModelBR(){Name = ""}, new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Error // or Ignore
+        });JsonConvert.SerializeObject(new BlinkingLightStateBR(), new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Error // or Ignore
+        });
+
+        // Assert
+        Assert.NotNull(json);
+    }
+}
---
+++ Frames.Tests/BlinkingLightBR/SerializationTest.cs
@@ -0,0 +1,22 @@
+﻿using Frames.Tests.BlinkingLightBR;
+using Newtonsoft.Json;
+
+public class SerializationTest
+{
+    [Fact]
+    public void TestSerialization()
+    {
+        // Arrange
+        var arena = new CArena();
+        
+        // Act
+        var json = JsonConvert.SerializeObject(arena, new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Error // or Ignore
+        });
+        
+        // Assert
+        Assert.NotNull(json);
+        
+    }
+}
